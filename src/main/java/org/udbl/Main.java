0a06c7d095062d34010d0a4e379baed3e5112706
Yves NDET <125270591+YVESCREDIA@users.noreply.github.com>
2025-11-25 15:18:16 +0200

    test dans main enregistrement fichier

--- src/main/java/org/udbl/Main.java
+++ src/main/java/org/udbl/Main.java
@@ -1,8 +1,13 @@
 package org.udbl;
 
+import org.udbl.dao.AnimalFileRepository;
 import org.udbl.entity.Animal;
+import org.udbl.entity.ChauveSouris;
 import org.udbl.entity.Perroquet;
 import org.udbl.gestion.AgeValidatorException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,7 +16,13 @@ public class Main {
 
 
     Animal animal=new Perroquet(5,"XXW",8,0,"Coco",4);
-
+    Animal animal2=new ChauveSouris(5,"eec",4,5,"eer","brun");
+    //enregistrement
+    AnimalFileRepository animalFileRepository=new AnimalFileRepository("anim.txt");
+    List<Animal>animals=new ArrayList<>();
+    animals.add(animal);
+    animals.add(animal2);
+    animalFileRepository.save(animals);
     System.out.println(animal.toString());
 
 
--- src/main/java/org/udbl/dao/AnimalFileRepository.java
+++ src/main/java/org/udbl/dao/AnimalFileRepository.java
@@ -1,4 +1,61 @@
 package org.udbl.dao;
+import org.udbl.entity.Animal;
 
-public class AnimalFileRepository {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+
+public class AnimalFileRepository implements AnimalRepository {
+    String filename;
+    public AnimalFileRepository(String filename){
+        this.filename=filename;
+    }
+    @Override
+    public void save(List<Animal> animaux) {
+        try {
+            ObjectOutputStream enregistreurObjet;
+            FileOutputStream enregistreuFichier=new FileOutputStream(filename);
+            enregistreurObjet=new ObjectOutputStream(enregistreuFichier);
+            enregistreurObjet.writeObject(animaux);
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+    @Override
+    public List<Animal> load() {
+        try {
+            ObjectInputStream chargeur;
+            FileInputStream chargeurFichier=new FileInputStream(filename);
+            chargeur=new ObjectInputStream(chargeurFichier);
+            List<Animal>animaux=(List<Animal>) chargeur.readObject();
+            return animaux;
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
+
+    @Override
+    public void update(Animal animal) {
+
+    }
+
+    @Override
+    public void delete(Animal animal) {
+
+    }
 }
